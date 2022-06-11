2a5528d04b0fd78eb10e04c2e86146245a15eedf
NDICHU <ndichumuriithi@gmail.com>
2022-06-11 11:14:02 +0300

    Completed assignment

--- .history/parrot_20220611111326.rb
+++ .history/parrot_20220611111326.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(kasuku="Squawk!")
-    puts "#{kasuku}"
+    puts "#"
     return kasuku
 end
 parrot("Pretty bird!")
